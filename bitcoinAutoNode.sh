8b6ec993dd51c7af1cf7b14429af863ca91c56e5
dell1503 <dell1503@users.noreply.github.com>
2019-01-31 08:05:04 +0100

    Crontab for user

--- bitcoinAutoNode.sh
+++ bitcoinAutoNode.sh
@@ -12,27 +12,25 @@ echo "########### Creating Swap"
 
 echo "########### Creating config"
 config="/usr/bin/bitcoin.conf"
+datadir="/media/bitcoin"
+
 sudo touch $config
 sudo echo "server=1" > $config
 sudo echo "daemon=1" >> $config
+sudo echo "datadir="$datadir >> $config
 sudo echo "connections=40" >> $config
 randUser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 randPass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 sudo echo "rpcuser=$randUser" >> $config
 sudo echo "rpcpassword=$randPass" >> $config
 
+echo "############ Create Folders & Premissions"
+sudo mkdir $datadir
+sudo chmod -R a+rwx /home/ubuntu/.bitcoin
+sudo chmod -R a+rwx /media/bitcoin
 
 echo "########### Setting up autostart (cron)"
 
-sudo touch /root/script.sh
-sudo echo "/usr/bin/bitcoind -daemon -conf=/usr/bin/bitcoin.conf" >> /root/script.sh
-sudo chmod u+x /root/script.sh
-echo "sh /root/script.sh &" >> /etc/rc.local
-
-# Ich dänke de Befehl sött nöd is Config file gschriebe werde sondern usgfüehrt werde oder? #
-sudo echo "@reboot /usr/bin/bitcoind -daemon -conf=/usr/bin/bitcoin.conf" >> /etc/crontab
-#sudo @reboot /usr/bin/bitcoind -daemon -conf=/usr/bin/bitcoin.conf #>> /etc/crontab
-
-/usr/bin/bitcoind -daemon -conf=/usr/bin/bitcoin.conf
+(crontab -l 2>/dev/null; echo "@reboot /usr/bin/bitcoind -daemon -conf=/usr/bin/bitcoin.conf") | crontab -
 
 # reboot
